c21cea08caa229c016aecbe1a96d2f145d52a889
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-09 13:17:47 +0330

    Add MongoDB dependency and implement API for new meetup creation

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,5 +1,19 @@
-function handler(){
+// /api/new-meetup
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
 
+    const client = await MongoClient.connect(
+      "mongodb+srv://motahare:m2541380@cluster0.6wqtz.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+    console.log(result);
+    client.close();
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
 }
 
 export default handler;
