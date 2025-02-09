f22df8aa207bebfa3a3c59af4962e4673a884090
moti.vatandoust <moti.vatandoust@gmail.com>
2025-02-09 12:20:41 +0330

    Implement dynamic data fetching for MeetupDetails using getStaticProps

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,15 +1,41 @@
 import MeetupDetail from "@/components/meetups/MeetupDetail";
 
-
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      title="A First Meetup"
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
-      address="Some address 5, 12345 Some City"
-      description="This is a first meetup!"
+      title={props.meetupData.title}
+      image={props.meetupData.image}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      { params: { meetupId: "m1" } },
+      { params: { meetupId: "m2" } },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "A First Meetup",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        address: "Some address 5, 12345 Some City",
+        description: "This is a first meetup!",
+      },
+    },
+  };
 }
 
 export default MeetupDetails;
